47ffa94bb4758b49bc25a36c79e3dd8bd7a22ad1
Shamanoff <shamanoff@gmail.com>
2018-02-14 11:21:59 +0200

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DialogDataExampleDialog, JsonworkComponent} from './jsonwork/jsonwork.co
 import {FetchdataService} from './fetchdata.service';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 const appRoutes: Routes = [
   {path: '', component: MatComponent},
   {path: 'json', component: JsonworkComponent},
@@ -40,6 +41,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     MaterialModule,
     HttpClientModule,
+    FlexLayoutModule
 
   ],
   providers: [FetchdataService],
